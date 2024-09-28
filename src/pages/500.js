105c82d8499d6ccd88681c9a97a11d3c1dd9f856
ai-image-game <image-game.ai@gmail.com>
2024-09-29 00:46:39 +0900

    500 에러 페이지 생성

---
+++ src/pages/500.js
@@ -0,0 +1,8 @@
+export default function ServerError() {
+    return (
+        <div>
+            <h1>500 - Server Error</h1>
+            <p>Something went wrong on our end. Please try again later.</p>
+        </div>
+    );
+}
