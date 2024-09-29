7c98268de59b051ce6e64c5cfafd5f7000ecebd0
ai-image-game <image-game.ai@gmail.com>
2024-09-29 22:29:04 +0900

    연결이 끊어졌을 때 websocket 수정

--- src/common/Websocket.js
+++ src/common/Websocket.js
@@ -48,13 +48,20 @@ export function initSocket(imageGameInfo, setImageGameInfo, processImageGameInfo
         console.error('Broker reported error: ' + frame.headers['message']);
         console.error('Additional details: ' + frame.body);
     };
+
+    client.onWebSocketClose = function(evt) {
+        isConnected = false;
+        console.log(evt);
+    };
+
     client.activate();
 
     return () => {
         if (client) {
             client.deactivate()
                 .then(() => {
-                    console.log('Disconnected from STOMP server');
+                    console.log('Disconnected from server');
+                    window.location.href = "/";
                 });
         }
     };
@@ -66,6 +73,9 @@ export function guess (guessInfo) {
             destination: '/image-game/guess',
             body: JSON.stringify(guessInfo),
         });
+    } else {
+        alert("Disconnected from Server");
+        window.location.href = "/";
     }
 }
 
