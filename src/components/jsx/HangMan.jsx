b49eafd5df7a1efd461b6a332374bc13bc677509
ai-image-game <image-game.ai@gmail.com>
2024-10-31 23:48:39 +0900

    행맨 이미지로 변경

--- src/components/jsx/HangMan.jsx
+++ src/components/jsx/HangMan.jsx
@@ -22,13 +22,13 @@ function HangMan({ guessInfo }) {
     }, []);
 
     return (
-        <div className={`${styles.hangmanArea} ${wrongCount === 6 ? styles.killed : ''}`}>
+        <div className={`${styles.hangmanArea} ${wrongCount >= 6 ? styles.killed : ''}`}>
             {wrongCount === 1 && <img src="/images/hangman/hangman_head_small.png" alt="hangman_head"/>}
             {wrongCount === 2 && <img src="/images/hangman/hangman_arms_small.png" alt="hangman_arms"/>}
             {wrongCount === 3 && <img src="/images/hangman/hangman_legs_small.png" alt="hangman_legs"/>}
             {wrongCount === 4 && <img src="/images/hangman/hangman_stick_small.png" alt="hangman_stick"/>}
             {wrongCount === 5 && <img src="/images/hangman/hangman_stick_up_small.png" alt="hangman_stick_up"/>}
-            {wrongCount === 6 && <img src="/images/hangman/hangman_small.png" alt="hangman"/>}
+            {wrongCount >= 6 && <img src="/images/hangman/hangman_small.png" alt="hangman"/>}
         </div>);
 }
 
