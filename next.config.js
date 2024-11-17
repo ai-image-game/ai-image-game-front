a125b687abbd330a4b147acb7f0f8a37d872e009
ai-image-game <image-game.ai@gmail.com>
2024-11-17 15:30:51 +0900

    production용 환경설정

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.resolve(__dirname, '.env.production') });
+
+console.log("###### ENVIRONMENT VARIABLES DURING BUILD:", process.env);
+
+module.exports = {
+    reactStrictMode: true,
+};
