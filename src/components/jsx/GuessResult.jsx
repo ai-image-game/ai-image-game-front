f78e162f4da404e97fe5ff762d32534447c39101
ai-image-game <image-game.ai@gmail.com>
2024-11-27 00:14:17 +0900

    prefix, postfix 첫 글자 대문자로 변경

--- src/components/jsx/GuessResult.jsx
+++ src/components/jsx/GuessResult.jsx
@@ -3,7 +3,11 @@ import styles from '../css/GuessResult.module.css';
 function GuessResult({questionInfo}) {
     return (
         <div className={styles.guessInputs}>
-            <span className={styles.prefix}>{questionInfo.prefix}</span>
+            {questionInfo.prefix && <span className={styles.prefix}>
+                {questionInfo.prefix.split("").map((char, index) => (
+                    <>{index === 0 ? char.toUpperCase() : char}</>
+                ))}
+            </span>}
             {questionInfo.maskedAnswer.split("").map((char, index) => (
                 <span key={index} className={`${char === '*' ? styles.guessInput : char === ' ' ? styles.blank : styles.guessCorrect}`}>
                 {char === '*' ? ' ' : (index === 0 || questionInfo.maskedAnswer[index - 1] === ' ' ? char.toUpperCase() : char)}
@@ -13,7 +17,12 @@ function GuessResult({questionInfo}) {
                     ))}
             </span>
             ))}
-            <span className={styles.postfix}>{questionInfo.postfix}</span>
+            {questionInfo.postfix && <span className={styles.postfix}>
+                {questionInfo.postfix.split("").map((char, index) => (
+                    <>{index === 0 || questionInfo.postfix[index -1] === ' ' ? char.toUpperCase() : char}</>
+                ))}
+            </span>
+            }
         </div>
     );
 }
