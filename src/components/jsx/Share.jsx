aa157ce9dd7106bf20fe11b60baa7fac7ffd997e
ai-image-game <image-game.ai@gmail.com>
2025-01-01 00:42:51 +0900

    공유기능 추가

--- src/components/jsx/Share.jsx
+++ src/components/jsx/Share.jsx
@@ -8,34 +8,23 @@ function Share({stageStatus, url}) {
             console.error('Could not copy text: ', err);
         });
     }
-
-    function alertPreparing() {
-        alert("This feature is currently being prepared.");
-    }
-    return (/*
-        <div className={`${styles.shareArea} ${stageStatus.share ? styles.shareAreaPink : ''}`}>
-            <a target="_blank" rel="noreferrer"
-               href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">
-                <img src="facebook_icon.png" className={`${styles.logo}`}/>
-            </a>
-            <a target="_blank" rel="noreferrer"
-               href="https://twitter.com/intent/tweet?url=http://localhost:3000&text=Guess What? green _______">
-                <img src="x-black_icon.png" className={styles.logo}/>
-            </a>
-            <a target="_blank" rel="noreferrer"
-               href="https://api.whatsapp.com/send?text=여기에%20공유할%20텍스트를%20입력하세요%20{{current-page-url}}"
-               target="_blank">
-                <img src="whats-app_icon.png" className={styles.logo}/>
-            </a>
-            <img src="link_icon.png" className={`${styles.logo} ${styles.copy}`} onClick={copyTextToClipboard}/>
-        </div>*/
-
-        <div className={`${styles.shareArea} ${stageStatus.share ? styles.shareAreaPink : ''}`}>
-                <img src="facebook_icon.png" alt="facebook" className={`${styles.logo}`} onClick={alertPreparing}/>
-                <img src="x-black_icon.png" alt="x" className={styles.logo}  onClick={alertPreparing}/>
-                <img src="whats-app_icon.png" alt="whats app" className={styles.logo} onClick={alertPreparing}/>
-                <img src="link_icon.png" alt="copy" className={`${styles.logo} ${styles.copy}`} onClick={alertPreparing}/>
-        </div>
+    return (
+            <div className={`${styles.shareArea} ${stageStatus.share ? styles.shareAreaPink : ''}`}>
+                <a target="_blank" rel="noreferrer"
+                   href={`'https://www.facebook.com/sharer/sharer.php?u='${url}';src=sdkpreparse'`}>
+                    <img src="facebook_icon.png" className={`${styles.logo}`}/>
+                </a>
+                <a target="_blank" rel="noreferrer"
+                   href={`'https://twitter.com/intent/tweet?url='${url}'&text=Guess What It Is!'`}>
+                    <img src="x-black_icon.png" className={styles.logo}/>
+                </a>
+                <a target="_blank" rel="noreferrer"
+                   href={`'https://api.whatsapp.com/send?text=Guess What It Is! ${url}'`}
+                   target="_blank">
+                    <img src="whats-app_icon.png" className={styles.logo}/>
+                </a>
+                <img src="link_icon.png" className={`${styles.logo} ${styles.copy}`} onClick={copyTextToClipboard}/>
+            </div>
     )
 }
 
